c8aa1058538635c5257bbb609efa44f7636c2687
Pedro Joao <saintjohn608@gmail.com>
2022-12-05 18:41:15 -0300

    atualizando

---
+++ aulasJS/Loops/laçofor-loopfor/lflp.js
@@ -0,0 +1,10 @@
+/* for(let i = 1; i <= 5; i++) {
+    console.log('Estou aprendendo',i)
+}
+*/
+
+for(let i = 1; i <= 100; i++) {
+    if(i % 2 !== 0) {
+        console.log(i)
+    }
+}
---
+++ aulasJS/Loops/laçowhile-loopwhile/lwlw.js
@@ -0,0 +1,10 @@
+// While loop
+
+let i = 5;
+
+while (i >= 1) {
+    if(i % 2 !== 0){
+        console.log(i)
+    }
+    i--
+}
