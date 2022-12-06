e3c2b70eedc7122d9e57cb950ab52ee04345357c
Pedro Joao <saintjohn608@gmail.com>
2022-12-06 16:52:53 -0300

    atualizando

---
+++ minis-projetos/maximo-entre-dois-valores/mdv.js
@@ -0,0 +1,9 @@
+// Escreva um funcao que usa 2 numeros e retorna o maior entre eles
+
+let valorMaior = max(10,10)
+console.log(valorMaior)
+
+function max(numero1,numero2) {
+    return numero1 > numero2 ? numero1 : numero2
+}
+
